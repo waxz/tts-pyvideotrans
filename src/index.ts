9455d58aafef862a0813b3d0d5488d4c43a10d35
waxz <waxz@github.com>
2025-06-10 10:15:20 +0000

    add oprnai.fm

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ let endpoint = null;
 let clientId = "";
 
 
-const TOKEN_REFRESH_BEFORE_EXPIRY = 3 * 60;  
+const TOKEN_REFRESH_BEFORE_EXPIRY = 3 * 60;
 let tokenInfo = {
     endpoint: null,
     token: null,
@@ -17,15 +17,30 @@ addEventListener("fetch", event => {
     event.respondWith(handleRequest(event.request));
 });
 
+//utils
+function encodeSpaceAsPlus(str) {
+    return encodeURIComponent(str).replace(/%20/g, '+');//.replace(/%3D/g, '=').replace(/%26/g, '&');
+
+}
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+//utils
+
 async function handleRequest(request) {
     if (request.method === "OPTIONS") {
         return handleOptions(request);
     }
-    
-  
+
+
+
+
     const authHeader = request.headers.get("authorization") || request.headers.get("x-api-key");
-    const apiKey = authHeader?.startsWith("Bearer ") 
-        ? authHeader.slice(7) 
+    const apiKey = authHeader?.startsWith("Bearer ")
+        ? authHeader.slice(7)
         : null;
 
     // 只在设置了 API_KEY 的情况下才验证              
@@ -48,30 +63,41 @@ async function handleRequest(request) {
 
     const requestUrl = new URL(request.url);
     const path = requestUrl.pathname;
-    
+    var getAudio;
+
+    if (path === "/openai-fm/v1/audio/speech") {
+        getAudio = getOpenfmAudioChunk;
+    }
+
     if (path === "/v1/audio/speech") {
+        getAudio = getEdgeAudioChunk;
+    }
+
+
+    if (getAudio) {
         try {
             const requestBody = await request.json();
-            const { 
+            const {
                 model = "tts-1",
                 input,
                 voice = "zh-CN-XiaoxiaoNeural",
                 response_format = "mp3",
                 speed = '1.0',
-                volume='0',
+                volume = '0',
                 pitch = '0', // 添加 pitch 参数，默认值为 0
                 style = "general"//添加style参数，默认值为general
             } = requestBody;
 
-            let rate = parseInt(String( (parseFloat(speed)-1.0)*100) );
-            let numVolume = parseInt( String(parseFloat(volume)*100) );
-            let numPitch = parseInt(pitch); 
+            let rate = parseInt(String((parseFloat(speed) - 1.0) * 100));
+            let numVolume = parseInt(String(parseFloat(volume) * 100));
+            let numPitch = parseInt(pitch);
             const response = await getVoice(
-                input, 
-                voice, 
-                rate>=0?`+${rate}%`:`${rate}%`,
-                numPitch>=0?`+${numPitch}Hz`:`${numPitch}Hz`,
-                numVolume>=0?`+${numVolume}%`:`${numVolume}%`,
+                getAudio,
+                input,
+                voice,
+                rate >= 0 ? `+${rate}%` : `${rate}%`,
+                numPitch >= 0 ? `+${numPitch}Hz` : `${numPitch}Hz`,
+                numVolume >= 0 ? `+${numVolume}%` : `${numVolume}%`,
                 style,
                 "audio-24khz-48kbitrate-mono-mp3"
             );
@@ -112,21 +138,47 @@ async function handleOptions(request) {
     });
 }
 
-async function getVoice(text, voiceName = "zh-CN-XiaoxiaoNeural", rate = '+0%', pitch = '+0Hz', volume='+0%',style = "general", outputFormat = "audio-24khz-48kbitrate-mono-mp3") {
-    try {
-        const maxChunkSize = 2000;  
-        const chunks = text.trim().split("\n");
+function splitAndMerge(str, chunkSize, determin) {
+    const words = str.split(determin);
+    const result = [];
+    let currentChunk = [];
+    let currentLength = 0;
 
+    for (const word of words) {
+        if (currentLength + word.length + (currentChunk.length > 0 ? 1 : 0) <= chunkSize) {
+            currentChunk.push(word);
+            currentLength += word.length + (currentChunk.length > 1 ? 1 : 0);
+        } else {
+            if (currentChunk.length > 0) {
+                result.push(currentChunk.join(' '));
+            }
+            currentChunk = [word];
+            currentLength = word.length;
+        }
+    }
+
+    if (currentChunk.length > 0) {
+        result.push(currentChunk.join(' '));
+    }
+
+    return result;
+}
+
+async function getVoice(getAudio, text, voiceName = "zh-CN-XiaoxiaoNeural", rate = '+0%', pitch = '+0Hz', volume = '+0%', style = "general", outputFormat = "audio-24khz-48kbitrate-mono-mp3") {
+    try {
+        const maxChunkSize = 2000;
+        const chunks = splitAndMerge(text.trim(), maxChunkSize, "\n");
+        console.log(`getVoice chunks: ${chunks}`)
 
         // 获取每个分段的音频
         //const audioChunks = await Promise.all(chunks.map(chunk => getAudioChunk(chunk, voiceName, rate, pitch, volume,style, outputFormat)));
-        let audioChunks=[]
-        while(chunks.length>0){
-            try{
-                let audio_chunk= await getAudioChunk(chunks.shift(), voiceName, rate, pitch, volume,style, outputFormat)
+        let audioChunks = []
+        while (chunks.length > 0) {
+            try {
+                let audio_chunk = await getAudio(chunks.shift(), voiceName, rate, pitch, volume, style, outputFormat)
                 audioChunks.push(audio_chunk)
 
-            }catch(e){
+            } catch (e) {
                 return new Response(JSON.stringify({
                     error: {
                         message: String(e),
@@ -144,7 +196,7 @@ async function getVoice(text, voiceName = "zh-CN-XiaoxiaoNeural", rate = '+0%', 
 
             }
         }
-       
+
 
         // 将音频片段拼接起来
         const concatenatedAudio = new Blob(audioChunks, { type: 'audio/mpeg' });
@@ -155,7 +207,7 @@ async function getVoice(text, voiceName = "zh-CN-XiaoxiaoNeural", rate = '+0%', 
             }
         });
 
-        
+
         return response;
 
     } catch (error) {
@@ -165,7 +217,7 @@ async function getVoice(text, voiceName = "zh-CN-XiaoxiaoNeural", rate = '+0%', 
                 message: error,
                 type: "api_error",
                 param: null,
-                code: "edge_tts_error "+voiceName
+                code: "edge_tts_error " + voiceName
             }
         }), {
             status: 500,
@@ -176,18 +228,80 @@ async function getVoice(text, voiceName = "zh-CN-XiaoxiaoNeural", rate = '+0%', 
         });
     }
 }
+async function getOpenfmAudioChunk(text, voiceName = "alloy", rate = "+0%", pitch = "+0Hz", volume = "+0%", style = "calm", outputFormat = "audio-24khz-48kbitrate-mono-mp3") {
 
+    let m = text.match(/\[(\d+)\]\s*?$/);
+    let slien = 0;
+    if (m && m.length == 2) {
+        slien = parseInt(m[1]);
+        text = text.replace(m[0], '')
+
+    }
+
+    const endpoint = "https://www.openai.fm/api/generate";
+
+    const style_promote = {
+        caml: `Voice Affect: Calm, composed, and reassuring; project quiet authority and confidence, BBC repoter host accent.
+
+Tone: Sincere, empathetic, and gently authoritative—express genuine apology while conveying competence.
+
+Pacing: Steady and moderate; unhurried enough to communicate care, yet efficient enough to demonstrate professionalism.
+
+Emotion: Genuine empathy and understanding; speak with warmth, especially during apologies ("I'm very sorry for any disruption...").
+
+Pronunciation: Clear and precise, emphasizing key reassurances ("smoothly," "quickly," "promptly") to reinforce confidence.
+
+Pauses: Brief pauses after offering assistance or requesting details, highlighting willingness to listen and support.`
+    }
+    const style_voice = {
+        alloy: "alloy"
+
+    }
+    const select_promote = style_promote[style] || style_promote["caml"];
+    const select_voice = style_voice[voiceName.toLocaleLowerCase()] || style_voice["alloy"];
+
+    const uuid = generateUUID();
+
+
+    const param = `input=${encodeSpaceAsPlus(text)}&voice=${encodeSpaceAsPlus(select_voice)}&prompt=${encodeSpaceAsPlus(select_promote)}&generation=${uuid}`;
+
+    const encodedUrl = param;
+    const endpointUrl = `${endpoint}?${encodedUrl}`;
+    const clientId = crypto.randomUUID().replace(/-/g, "");
+
+    const response = await fetch(endpointUrl, {
+        method: "GET",
+        headers: {
+            "Accept-Language": "zh-Hans",
+            "X-ClientVersion": "4.0.530a 5fe1dc6c",
+            "X-UserId": "0f04d16a175c411e",
+            "X-HomeGeographicRegion": "en-US",
+            "X-ClientTraceId": clientId,
+            "X-MT-Signature": await sign(endpointUrl),
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
+            "Content-Type": "application/json; charset=utf-8",
+            "Content-Length": "0",
+            "Accept-Encoding": "gzip"
+        }
+    });
+    if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Openai-fm TTS API error: ${response.status} ${errorText}`);
+    }
+
+    return response.blob();
+}
 
 
 //获取单个音频数据
-async function getAudioChunk(text, voiceName, rate, pitch,volume, style, outputFormat='audio-24khz-48kbitrate-mono-mp3') {
+async function getEdgeAudioChunk(text, voiceName, rate, pitch, volume, style, outputFormat = 'audio-24khz-48kbitrate-mono-mp3') {
     const endpoint = await getEndpoint();
     const url = `https://${endpoint.r}.tts.speech.microsoft.com/cognitiveservices/v1`;
-    let m=text.match(/\[(\d+)\]\s*?$/);
-    let slien=0;
-    if(m&&m.length==2){
-      slien=parseInt(m[1]);
-      text=text.replace(m[0],'')
+    let m = text.match(/\[(\d+)\]\s*?$/);
+    let slien = 0;
+    if (m && m.length == 2) {
+        slien = parseInt(m[1]);
+        text = text.replace(m[0], '')
 
     }
     const response = await fetch(url, {
@@ -198,7 +312,7 @@ async function getAudioChunk(text, voiceName, rate, pitch,volume, style, outputF
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
             "X-Microsoft-OutputFormat": outputFormat
         },
-        body: getSsml(text, voiceName, rate,pitch,volume, style,slien)
+        body: getSsml(text, voiceName, rate, pitch, volume, style, slien)
     });
 
     if (!response.ok) {
@@ -210,11 +324,11 @@ async function getAudioChunk(text, voiceName, rate, pitch,volume, style, outputF
 
 }
 
-function getSsml(text, voiceName, rate, pitch,volume,style,slien=0) {
-   let slien_str='';
-   if(slien>0){
-    slien_str=`<break time="${slien}ms" />`
-   }
+function getSsml(text, voiceName, rate, pitch, volume, style, slien = 0) {
+    let slien_str = '';
+    if (slien > 0) {
+        slien_str = `<break time="${slien}ms" />`
+    }
     return `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" version="1.0" xml:lang="zh-CN"> 
                 <voice name="${voiceName}"> 
                     <mstts:express-as style="${style}"  styledegree="2.0" role="default" > 
@@ -228,7 +342,7 @@ function getSsml(text, voiceName, rate, pitch,volume,style,slien=0) {
 
 async function getEndpoint() {
     const now = Date.now() / 1000;
-    
+
     if (tokenInfo.token && tokenInfo.expiredAt && now < tokenInfo.expiredAt - TOKEN_REFRESH_BEFORE_EXPIRY) {
         return tokenInfo.endpoint;
     }
@@ -236,7 +350,7 @@ async function getEndpoint() {
     // 获取新token
     const endpointUrl = "https://dev.microsofttranslator.com/apps/endpoint?api-version=1.0";
     const clientId = crypto.randomUUID().replace(/-/g, "");
-    
+
     try {
         const response = await fetch(endpointUrl, {
             method: "POST",
@@ -261,7 +375,7 @@ async function getEndpoint() {
         const data = await response.json();
         const jwt = data.t.split(".")[1];
         const decodedJwt = JSON.parse(atob(jwt));
-        
+
         tokenInfo = {
             endpoint: data,
             token: data.t,
@@ -291,10 +405,10 @@ function addCORSHeaders(response) {
 
 function makeCORSHeaders() {
     return {
-        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, x-api-key",
-        "Access-Control-Max-Age": "86400"  
+        "Access-Control-Max-Age": "86400"
     };
 }
 
@@ -348,7 +462,7 @@ function dateFormat() {
 async function fetchWithTimeout(url, options, timeout = 30000) {
     const controller = new AbortController();
     const id = setTimeout(() => controller.abort(), timeout);
-    
+
     try {
         const response = await fetch(url, {
             ...options,
